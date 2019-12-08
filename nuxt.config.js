aea4e9c78c9adceff31bad9254941d56bdbdfceb
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-08 15:56:43 +0800

    edit: [ 全域 SEO ] 新增基本 Meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,28 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "甜點電商",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
+      { "http-equiv": "X-UA-Compatible", content: "ie=edge" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "shortcut icon",
+        href: "~/img/favicon_48.ico",
+        type: "image/x-icon"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+        integrity:
+          "sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU",
+        crossorigin: "anonymous"
+      }
+    ]
   },
+
   /*
    ** Customize the progress-bar color
    */
