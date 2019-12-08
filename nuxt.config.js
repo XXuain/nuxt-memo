e4aeeb35b4b41acabbf720b4a34030a6d3728f70
Yellow Huang <yellow.huang@silkrode.com.tw>
2019-12-08 15:57:23 +0800

    edit: [ 全域 SEO ] 基本 SEO、SMO(對 Facebook) 所需資訊

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { "http-equiv": "X-UA-Compatible", content: "ie=edge" }
+      { "http-equiv": "X-UA-Compatible", content: "ie=edge" },
+      { property: "og:title", content: "甜點電商" },
+      { property: "og:description", content: "六角學院的甜點電商" },
+      { property: "og:site_name", content: "甜點電商" },
+      { property: "og:locale", content: "zh_TW" }
     ],
     link: [
       {
